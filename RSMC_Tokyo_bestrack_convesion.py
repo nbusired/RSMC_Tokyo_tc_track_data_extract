9b35c5fcaacb5abc6a1b064816e50d1b14fe1111
Nanda Kishore Reddy <49005345+nbusired@users.noreply.github.com>
2023-03-31 15:40:35 +0900

    Add files via upload

---
+++ RSMC_Tokyo_bestrack_convesion.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+
+tcname = str(input("Write typhoon name: "))
+fileinput = str(input("Provide csv file name: "))
+if not ".csv" in fileinput:
+  fileinput += ".csv"
+
+rsmc = pd.read_csv(fileinput, header=None)
+
+yy = []
+mm = []
+dd = []
+hh = []
+for dt in range(len(rsmc)):
+    yy.append(int(str(rsmc[0][dt])[:2]))
+    mm.append(int(str(rsmc[0][dt])[2:4]))
+    dd.append(int(str(rsmc[0][dt])[4:6]))
+    hh.append(int(str(rsmc[0][dt])[6:8]))
+
+year = 20
+yyyy = [int(str(year)+str(yy[yd])) for yd in range(len(yy))]
+hhhh = [str(int(hh[yd])) + ":00" for yd in range(len(hh))]
+
+date = ['{}/{}/{}'.format(mm[dt],dd[dt],yyyy[dt]) for dt in range(len(yyyy))]
+ddhh = ['{} {}'.format(date[dh], hhhh[dh]) for dh in range(len(date))]
+
+lat = rsmc[3]*0.1
+lon = rsmc[4]*0.1
+lat_rev = [float(f'{lat[ltt]:.2f}') for ltt in range(len(lat))]
+lon_rev = [float(f'{lon[lot]:.2f}') for lot in range(len(lon))]
+press = rsmc[5]
+ws = rsmc[6]
+
+typ_var = list(zip(ddhh, lat_rev, lon_rev, press, ws))
+df = pd.DataFrame(typ_var, columns=['date', 'latitude', 'longitude', 'pressure', 'wind(kt)'])
+df.to_csv(f'{tcname}_rsmc_tokyo_track.csv', index=False)
+
+
+
+
